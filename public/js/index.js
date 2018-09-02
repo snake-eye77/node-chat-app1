a45b9ac6ba2e5ebac8fafdfd872dcfa835d53e01
snake-eye77 <asarade164@gmail.com>
2018-09-02 14:52:21 +0530

    =broadcast

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,13 @@
 var socket=io();
 socket.on('connect',()=>{
     console.log('connected to server..')
-    socket.emit('Msg',{
-        from:'akash',
-        text:'what is going on???'
-    })
+  
 })
+socket.on('newMessage',function(newMsg){
+    console.log(newMsg);
+})
+
+    
 socket.on('disconnect',()=>{
     console.log('disconnect from server....')
-})
-socket.on('newMsg',function(msg){
-console.log('newmsg:',msg)
 })
